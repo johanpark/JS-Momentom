67abe651046662fb18c8e2232065fa763da213ed
pyhan23@gmail.com <pyhan23@gmail.com>
2020-09-09 21:50:43 +0900

    9/9

--- gretting.js
+++ gretting.js
@@ -1,7 +1,28 @@
 const form = document.querySelector(".js-form"),
-    input = form.querySelector("input");
+    input = form.querySelector("input"),
+    gretting =document.querySelector(".js-grettings"), 
+    USER_LS ="currentUser",
+    SHOWING_CN ="showing";
+
+function paintGretting(text){
+    form.classList.remove(SHOWING_CN);
+    gretting.className.add(SHOWING_CN);
+    gretting.innerText = `Hello ${text}`
+}
+
+function loadName()
+{
+    const currentUser  = localStorage.getItem("USRT_LS");
+    if(currentUser === NULL){
+       
+    }
+    else{
+        paintGretting(currentUser);
+    }
+}
+
 function init()
 {
- 
+ loadName();
 }
 init();
