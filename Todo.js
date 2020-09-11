0e01bd738906fa1e3ee6b4ef44efc1ad091782d5
johanpark <pyhan23@gmail.com>
2020-09-11 22:42:52 +0900

    9/11 list

--- Todo.js
+++ Todo.js
@@ -3,10 +3,23 @@ const Todoform = document.querySelector(".js-TodoForm"),
         TodoList = document.querySelector(".js-TodoList");
 
 const  TODO_LS = "Todos";
+const TodoArr = [];
 
 function PaintTodo(text)
 {
-    console.log(text);
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    delBtn.innerText ="❌";
+    const span = document.createElement("span");
+    span.innerText = text;
+    li.appendChild(delBtn);
+    li.appendChild(span);
+    TodoList.appendChild(li);
+    const TodoObj = {
+        text : text,
+        id : TodoArr.length +1
+        };
+        TodoArr.push(TodoObj);
 }
 function HandleSubmit(event){
     event.preventDefault();
@@ -14,17 +27,18 @@ function HandleSubmit(event){
     PaintTodo(currentVlaue);
     TodoInput.value ="";
     
+    
 }
 function loadTOdos(){
-    const Todos = localStorage.getItem(TODO_LS);
-    if(Todos !== null)
+    const LoadedTodos = localStorage.getItem(TODO_LS);
+    if(LoadedTodos !== null)
     {
-
     }
 }
 
 function init()
 {
     loadTOdos();
+    Todoform.addEventListener("submit", HandleSubmit);
 }
 init();
